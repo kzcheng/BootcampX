6029ee6d97f213e309148ae79c91755afde2b737
kzcheng <kevinzifancheng@gmail.com>
2020-02-03 15:52:42 -0800

    Completed BootcampX Queries II - JOIN

---
+++ 2_queries_joins/2.sql
@@ -0,0 +1,7 @@
+SELECT SUM(duration) as total_duration
+FROM assignment_submissions
+  JOIN students
+  ON students.id = student_id
+  JOIN cohorts
+  ON cohorts.id = students.cohort_id
+WHERE cohorts.name = 'FEB12'
